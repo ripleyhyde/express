be16c6a1e70c5f22f306c33d1d21f842f9764d04
Ripley <Bones@Hameds-MacBook-Pro.local>
2022-01-30 14:22:20 +0300

    little trouble with postman

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const app = express();
 //Import data.js file
 const products = require("./data");
 
+//App will now accept json objects (middleware)
+app.use(express.json());
+
 //Name URL
 //Function takes 2 parameters (req, res)
 //Execute the console log
@@ -16,5 +19,30 @@ app.get("/api/products", (req, res) => {
   res.json(products);
 });
 
-//localhost.8000
+//Method to create new product
+app.post("/api/products", (req, res) => {
+  //Give new product a new id
+  const id = products.length + 1;
+  //Deconstruct request body to add id to payload
+  const newProduct = { id, ...req.body };
+  //Add new object to array using push method
+  products.push(newProduct);
+  //Change response code
+  res.status(201);
+  //Message after request completion
+  res.json("new product added");
+});
+
+//Method to delete product
+//Add :id to URL as a changing variable
+app.delete("api/products/:id", (req, res) => {
+  //Set :id as a param
+  const { id } = req.params;
+
+  const newArray = products.filter((product) => product.id !== +id);
+  products = newArray;
+  res.status(204).res.json(newArray);
+});
+
+//localhost.8000 (must always be at the very bottom)
 app.listen(8000);
